adddffd3c27cb37e5b9411b06c185bb9ddab762d
kaprestsun <fiddle.june@gmail.com>
2025-03-17 06:19:04 +0000

    Fixed dijkstras

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -15,7 +15,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     distances[source] = 0;
     previous[source] = -1;
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
-    minHeap.push({source, 0});
+    minHeap.push({0, source});
     while(!minHeap.empty()){
         int u = minHeap.top().second;
         minHeap.pop();
@@ -27,7 +27,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
             if(!visited[v] && distances[u] + weight < distances[v]){
                 distances[v] = distances[u] + weight;
                 previous[v] = u;
-                minHeap.push({v, distances[v]});
+                minHeap.push({distances[v], v});
             }
         }
     }
--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -5,5 +5,28 @@
 #include <queue>
 #include <limits>
 #include <stack>
+using namespace std;
+
+int main(){
+    Graph G;
+    file_to_graph("src/small.txt", G);
+    // G.addEdge(0, 1, 4);
+    // G.addEdge(0, 2, 2);
+    // G.addEdge(1, 2, 5);
+    // G.addEdge(1, 3, 10);
+    // G.addEdge(2, 3, 3);
+    // G.addEdge(3, 4, 8);
+    // G.addEdge(4, 5, 4);
+    
+    int source = 0;
+    int destination = 5;
+    vector<int> previous(G.numVertices, -1);
 
 
+    vector<int> distances = dijkstra_shortest_path(G, source, previous);
+
+    vector<int> path = extract_shortest_path(distances, previous, destination);
+
+    print_path(path, distances[destination]);
+
+}
